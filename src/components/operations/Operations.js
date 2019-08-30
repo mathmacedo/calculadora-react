1c1c3c6ba0a5e3e4ab8900243e0101529ac19e85
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-08-29 22:37:08 -0300

    Criando states e função que gera botões para o componente operations

--- src/components/operations/Operations.js
+++ src/components/operations/Operations.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./Operations.css";
 
+import Button from "../button/Button";
+
 class Operations extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			buttons: ["+", "-", "/", "x"]
+		};
+	}
+
+	createButtonsElements = () => {
+		var buttonElements = [];
+		for (var i = 0; i < this.state.buttons.length; i++) {
+			buttonElements.push(
+				<Button
+					label={this.state.buttons[i]}
+					onClick={this.props.onClick}
+				/>
+			);
+		}
+
+		return buttonElements;
+	};
+
 	render() {
-		return <div></div>;
+		return <div id="operations">{this.createButtonsElements()}</div>;
 	}
 }
 
