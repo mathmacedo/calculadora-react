7b4f5256959447554d1083b07d0e8d9ab0c79fe3
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-08-29 22:23:54 -0300

    Adicionando funções e estados para criar botões dentro do componente numbers

--- src/components/numbers/Numbers.js
+++ src/components/numbers/Numbers.js
@@ -2,6 +2,21 @@ import React from "react";
 import "./Numbers.css";
 
 class Numbers extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			buttons: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
+		};
+	}
+
+	createButtonsElements = () => {
+		var buttonElements = [];
+		for (var i = 0; i < this.state.buttons; i++) {
+			buttonElements.push();
+		}
+	};
+
 	render() {
 		return <div></div>;
 	}
