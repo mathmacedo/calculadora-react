1387cb2d6ca905d41915c017e1267b08c8f337a9
Matheus Macedom <matheus.macedo1977@gmail.com>
2019-09-18 08:58:33 -0300

    Updates

---
+++ precache-manifest.f313e592333cad5d08bec7741fcfc8c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d123c1ef053d08dff0562769a968c0",
+    "url": "/mathmacedo/calculadora-react/index.html"
+  },
+  {
+    "revision": "c2158f3a4fe60a8625f5",
+    "url": "/mathmacedo/calculadora-react/static/css/main.3a12ffb3.chunk.css"
+  },
+  {
+    "revision": "334580ac53e6c7c14078",
+    "url": "/mathmacedo/calculadora-react/static/js/2.90cb6644.chunk.js"
+  },
+  {
+    "revision": "c2158f3a4fe60a8625f5",
+    "url": "/mathmacedo/calculadora-react/static/js/main.7907ae73.chunk.js"
+  },
+  {
+    "revision": "189a8de4005fd0183bb0",
+    "url": "/mathmacedo/calculadora-react/static/js/runtime~main.ab644c49.js"
+  }
+]);
